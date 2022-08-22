b8c792883584548441db16989255e1385d96f406
Daichu Flores <daichu101@gmail.com>
2022-08-22 15:15:18 -0500

    upload

--- TheMovieDB/Controller/PerfilViewController.swift
+++ TheMovieDB/Controller/PerfilViewController.swift
@@ -18,16 +18,4 @@ class PerfilViewController: UIViewController {
         
         lbluserName.text = nameUse
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
